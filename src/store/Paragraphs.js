97126c9bacc46dc6f49d6d0af3789fdd0e655957
Stanley Chibuike <chibuike.stanley50@gmail.com>
2022-07-24 20:49:20 +0100

    Added and removed unneccessary stuffs

---
+++ src/store/Paragraphs.js
@@ -0,0 +1,11 @@
+const Paragraphs = [
+    "The architectonic of pure reason depends on the Ideal of natural reason, but the objects in space and time constitute the whole content of the empirical objects in space and time.",
+    "There can be no doubt that our experience can be treated like our judgements, since none of the phenomena are problematic.",
+    "Therefore, it remains a mystery why the Categories are the clue to the discovery of our concepts, since none of the objects in space and time are hypothetical.",
+    "In the study of metaphysics, it is obvious that our faculties are a representation of the transcendental unity of apperception.",
+    "The Antinomies, in the case of the transcendental unity of apperception, exclude the possibility of applied logic; thus, the things in themselves stand in need to, in other words, the manifold.",
+    "Because of the relation between our experience and the objects in space and time, the paralogisms, however, exist in metaphysics.",
+    "As any dedicated reader can clearly see, the manifold is the key to understanding, so far as I know, the pure employment of space.",
+    "It remains a mystery why our judgements stand in need to the Transcendental Deduction.",
+    "The paralogisms, for these reasons, would be falsified."
+]
